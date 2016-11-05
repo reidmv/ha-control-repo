6c0007571b240bc9937552c5dbfcd19aaf2109cb
root <root@g3o09jzzhhtub2o.delivery.puppetlabs.net>
2016-11-04 18:46:28 -0700

    add /ha_test/control_repo/manifests/site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,1 @@
-site {
-  app1 { 'app1name':
-    nodes => {
-      Node['g3o09jzzhhtub2o.delivery.puppetlabs.net'] => [App1::Comp1['foo1'], ],
-      Node['i512wss1rey1n6x.delivery.puppetlabs.net'] => [App1::Comp2['foo2'], ],
-    },
-  }
-}
-
+node default { notify { 'alpha': } }
